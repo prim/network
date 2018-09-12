db3aaeaab4311f24b15037ac5a317fe72e1755bb
root <root@localhost.localdomain>
2018-09-12 11:02:18 -0400

    tap vpn

--- tap_vpn.py
+++ tap_vpn.py
@@ -13,8 +13,12 @@ import struct
 import os
 import fcntl
 
+def log(fmt, *args):
+    if args:
+        msg = fmt % (args)
+    print msg
+
 def main():
-    print sys.argv
     mode = sys.argv[1]
     if mode == "vpn":
         run_vpn_node()
@@ -30,21 +34,13 @@ def run_control_center():
     clients = {}
 
     while True:
-        print 1111111111111111, server_ip, server_port
         binary, addr = sock.recvfrom(1024)
-        print addr
         data = json.loads(binary)
         udp_addr = data["udp"]
-        print "----", data
         udp_addr = tuple(udp_addr)
-
-        if udp_addr in clients:
-            print "old", clients[udp_addr]
         clients[udp_addr] = addr
-
         packet = json.dumps([uaddr for uaddr in clients.iterkeys()])
         for _, remote_addr in clients.items():
-            print "111", repr(packet), repr(remote_addr)
             sock.sendto(packet, remote_addr)
 
 def run_vpn_node():
@@ -56,17 +52,17 @@ def run_vpn_node():
 
     # 连接TAP
     IFF_NO_PI = 0x1000
-    # IFF_TUN = 0x0001
+    IFF_TUN = 0x0001
     IFF_TAP = 0x0002
     TUNSETIFF = 0x400454ca
     tap = open("/dev/net/tun", "r+b")
     mode = IFF_NO_PI | IFF_TAP
-    print fcntl.ioctl(tap.fileno(), TUNSETIFF, struct.pack("16sH", tap_name, mode))
+    fcntl.ioctl(tap.fileno(), TUNSETIFF, struct.pack("16sH", tap_name, mode))
 
     # 连接 CONTROL CENTER
     control_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     packet = json.dumps({"udp":[local_ip, local_port]})
-    control_sock.sendto(packet, server_addr)
+    control_sock.sendto(packet, server_addr), server_addr
     
     # 监听本地UDP
     udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -82,79 +78,72 @@ def run_vpn_node():
 
     # 主循环
     while True:
-        print "tick"
         readables, _, _ = select.select([control_sock, udp_sock, tap], [], [], 1)
-        print "tick", readables
         for readable in readables:
             if readable is control_sock:
-                print 1
                 binary, _ = control_sock.recvfrom(1024)
-                print "repr", repr(binary)
                 data = json.loads(binary)
-                print "control", data
+                log("read from control_sock %s", data)
                 for uaddr in data:
                     uaddr = tuple(uaddr)
                     peers[uaddr] = Peer(uaddr)
 
             elif readable is tap:
-                print 2
-                # read from tap
+                # read from tap ethernet frame
                 binary = os.read(tap.fileno(), 1024)
-                # binary = tap.read(1024)
-                print "read from tap", repr(binary), len(binary)
-                # 读出来的binary是一个ethernet frame
-                # char[6] + char[6] + short
-                dest, source = binary[:6], binary[6:13]
-                print "read from tap", repr(dest), repr(source)
+                dest, source = binary[:6], binary[6:12]
+                log("read from tap %s %s %s", repr(dest), repr(source), repr(binary[12:]))
 
                 # frame routing
                 peer = None
-                for each in peers.itervalues():
-                    if each.mac == dest:
-                        peer = each
-                        break
+                if dest != "\xff\xff\xff\xff\xff\xff":
+                    for each in peers.itervalues():
+                        if each.mac == dest:
+                            log("frame routing to %s", each.addr)
+                            peer = each
+                            break
 
-                # encode frame
+                # encode frame TODO
                 frame_binary = binary
-                # encrypt 
+
+                # encrypt TODO
+
                 # send frame
                 if peer:
-                    print "sendto target", peer.addr
+                    log("send frame to peer %s", peer.addr)
                     udp_sock.sendto(frame_binary, peer.addr)
                 else:
-                    print "bs"
                     for addr in peers:
-                        if addr == local_addr:
-                            print "skip ", local_addr
-                            continue
-                        print "--------- send" , addr
-                        udp_sock.sendto(frame_binary, addr)
+                        if addr != local_addr:
+                            log("send frame to each %s", addr)
+                            udp_sock.sendto(frame_binary, addr)
 
             elif readable is udp_sock:
-                print 3
                 # receive frame
                 frame_binary, uaddr = udp_sock.recvfrom(1024)
-                # decrypt
-                # decode frame
-                binary = frame_binary
-                # mac learning
-                dest, source = binary[:6], binary[6:13]
-                print "read from udp", repr(binary)
-                print "read from udp", _
-                print "read from udp", repr(dest), repr(source)
 
-                os.write(tap.fileno(), binary)
-                continue
-                continue
+                # decrypt TODO
+
+                # decode frame TODO
+                binary = frame_binary
+
+                # mac learning
+                dest, source = binary[:6], binary[6:12]
+                log("read from udp %s %s %s", repr(dest), repr(source), repr(binary[12:]))
+
                 peer = None
                 for each in peers.itervalues():
                     if each.addr == uaddr:
                         peer = each
+                        peer.mac = source
+                        log("mac learning %s %s", uaddr, repr(source))
                         break
-                if peer is None:
-                    continue
-                print "~~~~~~~~", peer.__dict__
+
                 # write to tap
+                if peer is not None:
+                    log("write to tap %s", repr(binary))
+                    os.write(tap.fileno(), binary)
+
 
 if __name__ == "__main__":
     main()
